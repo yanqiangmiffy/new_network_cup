25ad2856020365c79095750e55c18671ba832ff9
yanqiangmiffy <1185918903@qq.com>
2018-09-29 19:05:26 +0800

    first

---
+++ 01_gbm.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python  
+# -*- coding:utf-8 _*-  
+""" 
+@Author:yanqiang 
+@File: 01_gbm.py 
+@Time: 2018/9/29 17:47
+@Software: PyCharm 
+@Description:
+"""
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
